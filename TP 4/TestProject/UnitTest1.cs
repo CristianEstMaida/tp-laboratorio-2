17afd121ee62018ed87b860371f6573de686932b
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2021-11-22 17:34:56 -0300

    Update UnitTest1.cs

--- TP 4/TestProject/UnitTest1.cs
+++ TP 4/TestProject/UnitTest1.cs
@@ -11,29 +11,14 @@ namespace TestProject1
         [TestMethod]
         public void GuardarLeerArchivo()
         {
-            //string archivo = "prueba";
-
-            List<float> lista = new List<float>()
-            {
-            //new List(10, new object())
-            };
+            List<float> lista = new List<float>();
             JsonFiler<List<float>> json = new JsonFiler<List<float>>();
-
-
-            //json.Guardar(archivo, lista);
-
-            //json.Leer(archivo, out juegosTest);
-
-            //Assert.AreEqual(juegos[0].Ubicacion, lista[0].Ubicacion);
         }
         [TestMethod]
         [ExpectedException(typeof(ErrorArchivosExcepction))]
         public void GuardarLeerArchivoException()
         {
-            List<float> juegos = new List<float>()
-            {
-                //new Juego(10, new object())
-            };
+            List<float> juegos = new List<float>();
             JsonFiler<List<float>> json = new JsonFiler<List<float>>();
 
             json.Guardar(" ", juegos);
