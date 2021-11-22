5f4553f261168398e7a4a1c5bd9c9c5dc13ebc75
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2021-11-22 10:04:40 -0300

    Add files via upload

---
+++ TP 3/WinFormsAppEscuela/Archivos/ErrorArchivosExcepction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    class ErrorArchivosExcepction : Exception
+    {
+        public ErrorArchivosExcepction(string message) : base(message) { }
+
+        public ErrorArchivosExcepction(string message, Exception inner) 
+            : base(message, inner) { }
+    }
+}
